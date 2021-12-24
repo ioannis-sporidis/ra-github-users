5d2bd61837793b7056b37cee9be57af3023161f0
Giannis Sporidis <the_fetaverse@Gianniss-MacBook-Pro.local>
2021-12-23 23:23:54 -0500

    Setup error page route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { Routes, Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="login" element={<Login />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
