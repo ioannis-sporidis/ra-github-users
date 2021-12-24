664bd9436666692826f4b2ad522bbc5c64c5f77e
Giannis Sporidis <the_fetaverse@Gianniss-MacBook-Pro.local>
2021-12-23 23:11:30 -0500

    Basic React Router setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { Routes, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="login" element={<Login />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
