a0ff355a1ad632556633bd3ee88828a0c961f6d2
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-12-31 13:39:36 -0500

    Login implementation but with no restricted access

--- src/index.js
+++ src/index.js
@@ -8,11 +8,17 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GithubProvider>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
